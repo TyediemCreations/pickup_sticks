a4c2e82ad5f19496f09265bd0dfe3c57d9dcd03e
John Speare <jtspeare@uwaterloo.ca>
2025-03-13 11:17:39 -0400

    Pass commandline arguments to game

--- pickup_sticks/__main__.py
+++ pickup_sticks/__main__.py
@@ -1,10 +1,46 @@
 """Main module for pickup_sticks."""
+import argparse
+import sys
+
 from game import Game
-from player import Player
+
+
+def setup_arguments():
+    parser = argparse.ArgumentParser(
+        prog="Pickup Sticks",
+        description="Someone dropped all these sticks, and it's your job to pick 'em up!",
+    )
+    parser.add_argument(
+        "-p", "--players",
+        type=int,
+        default=2,
+        help="Number of human players.",
+    )
+    parser.add_argument(
+        "-n", "--numsticks",
+        type=int,
+        default=50,
+        help="Number of sticks.",
+    )
+    parser.add_argument(
+        "-r", "--range",
+        nargs=2,
+        type=int,
+        default=(1,10),
+        help="Min/Max sticks that can be picked up per turn.",
+    )
+    args = parser.parse_args()
+    args.range = tuple(args.range)
+    return args
 
 
 def main():
-    game = Game()
+    args = setup_arguments()
+    game = Game(
+        num_players=args.players,
+        num_sticks=args.numsticks,
+        stick_range=args.range,
+    )
     game.play_game()
     return 0
 
